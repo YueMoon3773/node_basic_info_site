6c3cfb899148e677ab609a30134c015892ba0c1d
YueMoon3773 <giabaonguynhas@gmail.com>
2025-12-04 22:08:11 +0700

    Convert into Express JS

--- app.js
+++ app.js
@@ -1,47 +1,68 @@
-const http = require('node:http');
-const fs = require('node:fs/promises');
-const path = require('node:path');
+// const http = require('node:http');
+// const fs = require('node:fs/promises');
+// const path = require('node:path');
 
-const server = http.createServer(async (req, res) => {
-    // console.log('req url: ', req.url);
-    // console.log('req method: ', req.method);
+// const server = http.createServer(async (req, res) => {
+//     // console.log('req url: ', req.url);
+//     // console.log('req method: ', req.method);
 
-    // console.log(__dirname);
+//     // console.log(__dirname);
 
-    // res.setHeader('Content-Type', 'text/html');
+//     // res.setHeader('Content-Type', 'text/html');
 
-    let fileName = '';
-    let statusCode = 0;
-    switch (req.url) {
-        case '/home':
-            fileName = 'home.html';
-            statusCode = 200;
-            break;
-        case '/about':
-            fileName = 'about.html';
-            statusCode = 200;
-            break;
-        case '/contact':
-            fileName = 'contact.html';
-            statusCode = 200;
-            break;
-        default:
-            fileName = '404.html';
-            statusCode = 404;
-            break;
-    }
+//     let fileName = '';
+//     let statusCode = 0;
+//     switch (req.url) {
+//         case '/home':
+//             fileName = 'home.html';
+//             statusCode = 200;
+//             break;
+//         case '/about':
+//             fileName = 'about.html';
+//             statusCode = 200;
+//             break;
+//         case '/contact':
+//             fileName = 'contact.html';
+//             statusCode = 200;
+//             break;
+//         default:
+//             fileName = '404.html';
+//             statusCode = 404;
+//             break;
+//     }
 
-    const filePath = path.join(__dirname, 'views', fileName);
-    // console.log(fileName);
+//     const filePath = path.join(__dirname, 'views', fileName);
+//     // console.log(fileName);
 
-    res.writeHead(statusCode, { 'Content-Type': 'text/html; charset=utf-8' });
+//     res.writeHead(statusCode, { 'Content-Type': 'text/html; charset=utf-8' });
 
-    try {
-        const html = await fs.readFile(filePath, { encoding: 'utf8' });
-        res.end(html);
-    } catch (err) {
-        console.log(err);
-    }
+//     try {
+//         const html = await fs.readFile(filePath, { encoding: 'utf8' });
+//         res.end(html);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// });
+
+// server.listen(3000);
+
+const express = require('express');
+
+const app = express();
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    res.sendFile('./views/home.html', { root: __dirname });
 });
 
-server.listen(3000);
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname });
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile('./views/contact.html', { root: __dirname });
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', { root: __dirname });
+});
